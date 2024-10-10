0651e12a5cd205aa4f7b8c2d5792a11fd69a2ec5
Leandro Freires <dev.leandro.freires@gmail.com>
2024-10-10 09:23:16 -0300

    feat(controllers): create CalculatorController

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CalculatorApi.Controllers
+{
+    [Route("[controller]")]
+    public class CalculatorController : Controller
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
