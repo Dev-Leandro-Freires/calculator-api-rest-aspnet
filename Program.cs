91a01ec763f4d958623fe3cdf9cc037e2cf87b95
Leandro Freires <dev.leandro.freires@gmail.com>
2024-10-28 17:11:53 -0300

    docs(api): add swagger documentation

--- Program.cs
+++ Program.cs
@@ -1,17 +1,40 @@
+using Microsoft.OpenApi.Models;
+
+
+var appName = "Standard Calculator Api";
+var appVersion = "v1";
+var appDescription = "Standard calculator Api using ASP.NET";
+
+var openApiContact = new OpenApiContact
+{
+    Name = "Leandro Freires",
+    Url = new Uri("https://github.com/Dev-Leandro-Freires")
+};
+
+var openApiInfo = new OpenApiInfo
+{
+    Title = appName,
+    Version = appVersion,
+    Description = appDescription,
+    Contact = openApiContact
+};
+
+var swaggerUrn = $"/swagger/{appVersion}/swagger.json";
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( options => {options.SwaggerDoc(appVersion, openApiInfo);} );
+
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI(options => options.SwaggerEndpoint(swaggerUrn, $"{appName} - {appVersion}"));
 
 app.UseHttpsRedirection();
 
